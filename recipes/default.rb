aeda9ddeeb11dd4071f73bfcc8f3d3a6ac0d72be
Gavin Reynolds <gavin@chef.io>
2019-02-14 15:04:24 +0000

    Add AWS and Azure config, add test examples
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+user 'TestUser' do
+  password 'password'
+  action :create
+end
+
+group 'Administrators' do
+  members [
+    'TestUser',
+  ]
+  append true
+  action :modify
+end
+
+group 'FakeGroup' do
+  members [
+    'TestUser',
+  ]
+  append true
+  action :modify
+end
